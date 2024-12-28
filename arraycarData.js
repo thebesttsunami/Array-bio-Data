74ce132cd24a8844a3a9227e10d60f62a3b37424
The Best Tsunami <162686236+thebesttsunami@users.noreply.github.com>
2024-12-28 16:05:42 +0700

    Create arraycarData.js

---
+++ arraycarData.js
@@ -0,0 +1,27 @@
+const carData = [
+  {
+    id: 1,
+    brand: "Toyota",
+    model: "Camry",
+    year: 2022,
+    color: "Silver",
+    mileage: 20000
+  },
+  {
+    id: 2,
+    brand: "Honda",
+    model: "Civic",
+    year: 2020,
+    color: "Black",
+    mileage: 30000
+  },
+  {
+    id: 3,
+    brand: "Ford",
+    model: "Mustang",
+    year: 2019,
+    color: "Red",
+    mileage: 10000
+  }
+];
+console.log(carData);
